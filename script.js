5273b49ae1565054f7ac37b3b2c70a819f5e451e
MennyMo <molade779@gmail.com>
2021-11-19 07:46:55 +0100

    final commit

--- script.js
+++ script.js
@@ -1,34 +1,61 @@
 let el = document.getElementById("wrapper");
 let toggleButton = document.getElementById("menu-toggle");
-let users = document.querySelector('#users');
-let male = document.querySelector('#male');
-let female = document.querySelector('#female');
-let total = document.querySelector('#total');
-let getMale = document.querySelector('#get-male');
+let users = document.querySelector("#users");
+let male = document.querySelector("#male");
+let female = document.querySelector("#female");
+let total = document.querySelector("#total");
+let getMale = document.querySelector("#get-male");
 
+window.onload = async () => {
+   await getPage();
+   await fetchData();
+   await renderPage()
+}
+ 
 
 let user_data = [];
+let page = 1;
 
-let data = async() => {
-user_data = await  fetch('https://randomuser.me/api/?results=50')
-user_data = await user_data.json(); 
-user_data = user_data.results;
+function getPage() {
+  page = Number(location.hash.slice(1)) || page;
+  // console.log(page)
+}
+window.onhashchange = async () => {
+    await getPage();
+    await renderPage()
+ };
+//  console.log(page);
 
-total.textContent = user_data.length
-let numberOfMale = 0
-let numberOfFemale = 0
+async function fetchData() {
+  user_data = await fetch("https://randomuser.me/api/?results=50");
+  user_data = await user_data.json();
+  user_data = user_data.results;
 
+  // console.log(user_data);
+}
 
-for(let user of user_data){
-    if(user.gender === 'male') {
-        numberOfMale += 1
-    }    else {
-        numberOfFemale += 1
-    }       
-    
+function renderPage() {
+  let start = (page - 1) * 20;
+  let end = page * 20;
+  console.log(user_data);
+
+  let renderData = user_data.slice(start, end);
+  total.textContent = renderData.length;
+  let numberOfMale = 0;
+  let numberOfFemale = 0;
+
+  users.innerHTML = '';
+
+  for (let user of renderData) {
+    if (user.gender === "male") {
+      numberOfMale += 1;
+    } else {
+      numberOfFemale += 1;
+    }
+
     users.innerHTML += `<div class = "col-md-3 mb-3">
         <div class="card">
-                <img src= ${user.picture.large} class="card-img-top" alt="image">
+                <img src= ${user.picture.medium} class="card-img-top" alt="image">
                 <div class="card-body">
                   <h5 class="card-title">${user.name.title} ${user.name.first} ${user.name.last}</h5>
                   <p class="card-text">${user.gender}</p>
@@ -36,33 +63,12 @@ for(let user of user_data){
                 </div>
               </div>
         </div>
-   ` 
-
-//    getMale.addEventListener('click', () => {
-//     if(user.gender === 'male') {
-//         users.innerHTML += `<div class = "col-md-3 mb-3">
-//         <div class="card">
-//                 <img src= ${user.picture.large} class="card-img-top" alt="image">
-//                 <div class="card-body">
-//                   <h5 class="card-title">${user.name.title} ${user.name.first} ${user.name.last}</h5>
-//                   <p class="card-text">${user.gender}</p>
-//                   <p class="card-text"><small class="text-muted">${user.login.username}</small></p>
-//                 </div>
-//               </div>
-//         </div>
-//    ` 
-//     } return 
-
-// })
+   `;
+  }
+  male.textContent = numberOfMale;
+  female.textContent = numberOfFemale;
 }
-male.textContent = numberOfMale
-female.textContent = numberOfFemale
-// console.log(user_data);
-}
-data();
-
-
 
 toggleButton.onclick = function () {
-    el.classList.toggle("toggled");
+  el.classList.toggle("toggled");
 };
